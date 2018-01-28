591c50c910cea55d2c458742222ca906ba945558
andy santi <andysanti23@gmail.com>
2018-01-28 13:19:20 -0500

    Controllers & Modules

--- app/main.js
+++ app/main.js
@@ -1,2 +1,9 @@
-
+var app=angular.module('minmax',[]);
+app.controller('MinMaxCtrl',function($scope){
+    $scope.formModel={};
+    $scope.onSubmit = function(){
+        console.log("submitted");
+        console.log($scope.formModel);
+    };
+});
     
